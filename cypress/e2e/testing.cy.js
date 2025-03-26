bd715971b79247a4cd030972f3eb359edb6d0c39
Samuel23 <sam23akinwale@gmail.com>
2025-03-26 20:19:14 +0000

    Register form test

--- cypress/e2e/testing.cy.js
+++ cypress/e2e/testing.cy.js
@@ -4,9 +4,17 @@ describe('Test to launch a browser', () => {
     it('launch browser test', () => {
      
         cy.visit('https://naveenautomationlabs.com/opencart/index.php?route=account/register')
-        cy.get('#input-firstname').type('Sam')
-        cy.get('#input-lastname').type('Akin')
+        cy.get('#input-firstname').type('Samo')
+        cy.get('#input-lastname').type('Akinm')
+        cy.get('#input-email').type('kaziyin@yahoo.co.uk')
+        cy.get('#input-telephone').type('07428123477')
+        cy.get('#input-password').type('Simi12')
+        cy.get('#input-confirm').type('Simi12')
+        cy.get('input[type="checkbox"]').check()
+        cy.get('.btn.btn-primary').click()
+       
         
+    
 
 
     })
